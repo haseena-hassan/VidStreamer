59faf3b2ddff06d920cc38cc0d1c3214f2873252
haseena-hassan <haseena2199@gmail.com>
2024-04-29 16:49:54 +0530

    multer setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,26 @@ import { JwtModule } from '@nestjs/jwt';
 import { dbUrl, secret } from './utils/constants';
 import { join } from 'path/posix';
 
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { callbackify } from 'util';
+
 require('dotenv').config()
 
 @Module({
     imports: [
         MongooseModule.forRoot(dbUrl),
+
+        MulterModule.register({
+            storage: diskStorage({
+                destination: './public',
+                filename: (req, fileObject, multerCallback) => {
+                    const extension = fileObject.mimetype.split('/')[1];
+                    multerCallback(null, `${uuidv4()}-${Date.now()}-${extension}`);
+                }
+            }) 
+        }),
 
         JwtModule.register({
             secret,
