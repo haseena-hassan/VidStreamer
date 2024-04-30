1d8608df0f104690161c60a2a678ec63d6f17393
haseena-hassan <haseena2199@gmail.com>
2024-04-30 12:13:56 +0530

    configure middleware in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MulterModule } from '@nestjs/platform-express';
 import { JwtModule } from '@nestjs/jwt';
 
-
 import { dbUrl, secret } from './utils/constants';
 import { join } from 'path/posix';
 import { diskStorage } from 'multer';
 import { v4 as uuidv4 } from 'uuid';
-
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -21,10 +19,10 @@ import { UserService } from './service/user.service';
 import { Video, VideoSchema } from './model/video.schema';
 import { VideoController } from './controller/video.controller';
 import { VideoService } from './service/video.service';
+import { AuthMiddleware } from './app.middleware';
 
 
 
-require('dotenv').config()
 
 @Module({
     imports: [
@@ -37,7 +35,7 @@ require('dotenv').config()
                 destination: './public',
                 filename: (req, fileObject, multerCallback) => {
                     const extension = fileObject.mimetype.split('/')[1];
-                    multerCallback(null, `${uuidv4()}-${Date.now()}-${extension}`);
+                    multerCallback(null, `${uuidv4()}-${Date.now()}.${extension}`);
                 }
             }) 
         }),
@@ -54,4 +52,13 @@ require('dotenv').config()
     controllers: [AppController, UserController, VideoController],
     providers: [AppService, UserService, VideoService],
 })
-export class AppModule { }
+
+export class AppModule { 
+    
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(AuthMiddleware).exclude({
+            path: 'api/v1/video/:id',
+            method: RequestMethod.GET
+        })
+    }
+}
